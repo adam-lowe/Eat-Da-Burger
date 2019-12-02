7fcec69605c7e90bb14b2e5d47adee5c1cb5e5b3
Goldpm <lowedavidadam@gmail.com>
2019-12-02 09:57:41 -0500

    added connection confirmation

--- server.js
+++ server.js
@@ -21,3 +21,11 @@ var connection = mysql.createConnection({
   password: "rootroot",
   database: "burgers_db"
 });
+
+connection.connect(function(err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+    console.log("connected as id " + connection.threadId);
+  });
