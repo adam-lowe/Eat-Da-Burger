8282c9186dbf6038e229a672182d28487fc414be
Goldpm <lowedavidadam@gmail.com>
2019-12-02 09:55:30 -0500

    added express and handlebars setup

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
-const express = require("express")
+var exphbs = require("express-handlebars");
+var express = require("express");
+var mysql = require("mysql");
+
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "rootroot",
+  database: "burgers_db"
+});
