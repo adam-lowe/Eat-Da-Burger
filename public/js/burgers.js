663623ac130a8b74468eb8cbfe97190b065cc837
Goldpm <lowedavidadam@gmail.com>
2019-12-02 10:07:12 -0500

    added some fragmented code from quotes app

---
+++ public/js/burgers.js
@@ -0,0 +1,64 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+    $(".delburger").on("click", function(event) {
+      var id = $(this).data("id");
+  
+      // Send the DELETE request.
+      $.ajax("/api/burgers/" + id, {
+        type: "DELETE"
+      }).then(
+        function() {
+          console.log("deleted id ", id);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    $(".create-form").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+  
+      var newburger = {
+        author: $("#auth").val().trim(),
+        burger: $("#burg").val().trim()
+      };
+  
+      // Send the POST request.
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newburger
+      }).then(
+        function() {
+          console.log("created new burger");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    $(".update-form").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+  
+      var updatedburger = {
+        author: $("#auth").val().trim(),
+        burger: $("#quo").val().trim()
+      };
+  
+      var id = $(this).data("id");
+  
+      // Send the POST request.
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: updatedburger
+      }).then(
+        function() {
+          console.log("updated burger");
+          // Reload the page to get the updated list
+          location.assign("/");
+        }
+      );
+    });
+  });
+  
--- server.js
+++ server.js
@@ -29,3 +29,70 @@ connection.connect(function(err) {
     }
     console.log("connected as id " + connection.threadId);
   });
+
+  // Serve index.handlebars to the root route.
+app.get("/", function(req, res) {
+    connection.query("SELECT * FROM burgers_db;", function(err, data) {
+      if (err) {
+        return res.status(500).end();
+      }
+  
+      res.render("index", { burgers: data });
+    });
+  });
+  
+  app.post("/api/burgers", function(req, res) {
+    connection.query("INSERT INTO burgers (author, burger) VALUES (?, ?)", [req.body.author, req.body.burger], function(
+      err,
+      result
+    ) {
+      if (err) {
+        // If an error occurred, send a generic server failure
+        return res.status(500).end();
+      }
+  
+      // Send back the ID of the new burger
+      res.json({ id: result.insertId });
+    });
+  });
+  
+  app.delete("/api/burgers/:id", function(req, res) {
+    connection.query("DELETE FROM burgers WHERE id = ?", [req.params.id], function(err, result) {
+      if (err) {
+        // If an error occurred, send a generic server failure
+        return res.status(500).end();
+      }
+      else if (result.affectedRows === 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      }
+      res.status(200).end();
+  
+    });
+  });
+  
+  // Update a burger by an id and then redirect to the root route.
+  app.put("/api/burgers/:id", function(req, res) {
+    connection.query(
+      "UPDATE burgers SET author = ?, burger = ? WHERE id = ?",
+      [req.body.author, req.body.burger, req.params.id],
+      function(err, result) {
+        if (err) {
+          // If an error occurred, send a generic server failure
+          return res.status(500).end();
+        }
+        else if (result.changedRows === 0) {
+          // If no rows were changed, then the ID must not exist, so 404
+          return res.status(404).end();
+        }
+        res.status(200).end();
+  
+      }
+    );
+  });
+  
+  // Start our server so that it can begin listening to client requests.
+  app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
